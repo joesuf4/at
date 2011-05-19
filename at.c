62aeae9c837e8a1f01885c86e624b3c0ef18295e
Joe Schaefer <joesuf4@gmail.com>
2011-05-19 14:02:23 -0400

    deal properly with overflow from at_snprintf

--- at.c
+++ at.c
@@ -21,7 +21,7 @@
 
 #define AT_SUCCESS 0
 #define AT_EGENERAL 14
-
+#define min(a, b) ((a < b) ? a : b)
 
 int at_begin(at_t *t, int total)
 {
@@ -57,12 +57,12 @@ int at_comment(at_t *t, const char *fmt, va_list vp)
         return EINVAL;
 
 
-    end = b + rv;
+    end = b + min(rv, 250);
 
     buf[0] = '#';
     buf[1] = ' ';
 
-    if (rv == 250) {
+    if (rv >= 250) {
         end[-1] = '.';
         *end++ = '.';
         *end++ = '.';
@@ -136,9 +136,9 @@ void at_ok(at_t *t, int is_ok, const char *label, const char *file, int line)
     if (rv <= 0)
         exit(-1);
 
-    end = buf + rv;
+    end = buf + min(rv, 250);
 
-    if (rv == 250) {
+    if (rv >= 250) {
         *end++ = '.';
         *end++ = '.';
         *end++ = '.';
